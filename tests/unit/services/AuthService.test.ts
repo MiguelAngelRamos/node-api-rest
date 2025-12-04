cbcbf3bc0ef75140ae1347a5c6f3f6a9069dfec2
Miguel Ramos <return.infox@gmail.com>
2025-12-04 11:40:08 -0300

    codigo actualizado

--- tests/unit/services/AuthService.test.ts
+++ tests/unit/services/AuthService.test.ts
@@ -1,7 +1,8 @@
 import { IUserRepository } from "../../../src/domain/interfaces/IUserRepository";
 import { RegisterDto } from "../../../src/dtos/RegisterDto";
 import { AuthService } from "../../../src/services/AuthService";
-
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 jest.mock('bycryptjs');
 jest.mock('jsonwebtoken');
 
@@ -16,5 +17,19 @@ describe('AuthService - Unit test', () => {
       name: 'Sofia Testing'
     }
 
-    const hashedPassword = 'hashed_academynode_123'
+    const hashedPassword = 'hashed_academynode_123';
+    const mockToken = 'mock_jwt_token_123';
+
+    beforeEach(() => {
+      mockUserRepository = {
+        findByEmail: jest.fn(),
+        create: jest.fn()
+      }
+      authService = new AuthService(mockUserRepository);
+      (bcrypt.hash as jest.Mock).mockResolvedValue(hashedPassword);
+      (jwt.sign as jest.Mock).mockReturnValue(mockToken);
+    })
+
+   
+
 });
